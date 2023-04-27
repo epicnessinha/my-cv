b11d2ca1b37fbf7b9794838d4a8174aaa035b123
Vanessa <vanessa.ferreira.dev@gmail.com>
2023-04-27 21:55:44 +0100

    Unit test for JobItem component add

---
+++ src/components/JobItem/JobItem.test.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import JobItem from "./JobItem";
+
+describe("JobItem component", () => {
+  test("renders job item with the provided data", () => {
+    const testTitle = "Software Engineer";
+    const testCompany = "Test Company";
+    const testDescription = "Developing web applications";
+    const testStartDate = new Date(2020, 0, 1);
+
+    render(
+      <JobItem
+        title={testTitle}
+        company={testCompany}
+        description={testDescription}
+        startDate={testStartDate}
+      />
+    );
+
+    // Check if the title, company, and description elements are rendered correctly
+    expect(screen.getByText(testTitle)).toBeInTheDocument();
+    expect(screen.getByText(`Company: ${testCompany}`)).toBeInTheDocument();
+    expect(screen.getByText(`Job Functions: ${testDescription}`)).toBeInTheDocument();
+
+    // Check if the "Started" text is rendered with the correct elapsed time
+    const now = new Date();
+    const diff = now.getTime() - testStartDate.getTime();
+
+    const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+
+    const elapsedTimeText = `Started: ${years} years, ${hours} hours, and ${minutes} minutes ago`;
+    expect(screen.getByText(elapsedTimeText)).toBeInTheDocument();
+  });
+});
