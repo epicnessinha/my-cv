59bef5504831ab841b9eff9efbb0a087ef99f92a
Vanessa <vanessa.ferreira.dev@gmail.com>
2023-04-28 12:36:04 +0100

    Education page and StudyItem component add

---
+++ src/pages/Education/Education.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import StudyItem from "../../components/StudyItem/StudyItem";
+import "../../styles/main.scss";
+import "../../styles/studyItem.scss"
+
+interface ExperienceProps {
+  darkMode: boolean;
+}
+
+const Education: React.FC<ExperienceProps> = ({ darkMode }) => {
+  return (
+    <div className={`experience-container${darkMode ? " dark-mode" : ""}`}>
+      <h2>Academic Education</h2>
+      <div className="experience-item">
+        <StudyItem
+          title="Frontend Developer"
+          school="GeeksHub Academy"
+          course="Frontend development (React)"
+          startDate={new Date("2022-09-01")}
+        />
+      </div>
+      <div className="experience-item">
+        <StudyItem
+          title="FullStack Software Developer"
+          school="Academia de Código"
+          course="FullStack Software Development"
+          startDate={new Date("2022-09-01")}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Education;
