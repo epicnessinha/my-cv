6e7cabfff0e438645dfe7902b9058b9661f2fc5b
Vanessa <vanessa.ferreira.dev@gmail.com>
2023-04-30 17:21:13 +0100

    More design drafts and experiments in dark mode

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,46 +1,50 @@
-import React from "react";
+import React, { useContext } from "react";
 import ContactForm from "../ContactForm/ContactForm";
-import "../../styles/main.scss"
-import "../../components/Contacts/contacts.scss"
+import { DarkModeContext } from "../../context/DarkModeContext";
+import "../../styles/main.scss";
+import "./contacts.scss";
 
 const Contacts: React.FC = () => {
-   return (
-     <div className="contacts-container">
-       <h2>Contacts</h2>
-       <div className="contact-cards">
-         <div className="card phone-card">
-           <i className="fas fa-phone"></i>
-           <div className="card-info">
-             <p className="label">Phone</p>
-             <p>+351 910085402</p>
-           </div>
-         </div>
-         <div className="card email-card">
-           <i className="fas fa-envelope"></i>
-           <div className="card-info">
-             <p className="label">Email</p>
-             <p>vanessa.ferreira.dev@gmail.com</p>
-           </div>
-         </div>
-         <div className="card linkedin-card">
-           <i className="fab fa-linkedin"></i>
-           <div className="card-info">
-             <p className="label">LinkedIn</p>
-             <p>/vanessabio</p>
-           </div>
-         </div>
-         <div className="card github-card">
-           <i className="fab fa-github"></i>
-           <div className="card-info">
-             <p className="label">GitHub</p>
-             <p>/epicnessinha</p>
-           </div>
-         </div>
-       </div>
-       <h3 className="send-me">Send me a message:</h3>
+  const { darkMode } = useContext(DarkModeContext);
+
+  return (
+    <div className={`contacts-container ${darkMode ? "dark" : ""}`}>
+      <h2>Contacts</h2>
+      <div className="contact-cards">
+        <div className="card phone-card">
+          <i className="fas fa-phone"></i>
+          <div className="card-info">
+            <p className="label">Phone</p>
+            <p>+351 910085402</p>
+          </div>
+        </div>
+        <div className="card email-card">
+          <i className="fas fa-envelope"></i>
+          <div className="card-info">
+            <p className="label">Email</p>
+            <p>vanessa.ferreira.dev@gmail.com</p>
+          </div>
+        </div>
+        <div className="card linkedin-card">
+          <i className="fab fa-linkedin"></i>
+          <div className="card-info">
+            <p className="label">LinkedIn</p>
+            <p>/vanessabio</p>
+          </div>
+        </div>
+        <div className="card github-card">
+          <i className="fab fa-github"></i>
+          <div className="card-info">
+            <p className="label">GitHub</p>
+            <p>/epicnessinha</p>
+          </div>
+        </div>
+      </div>
+
+      <h3 className="send-me">Send me a message:</h3>
       <ContactForm />
-     </div>
-   );
- };
- 
- export default Contacts;
+    </div>
+  );
+};
+
+export default Contacts;
